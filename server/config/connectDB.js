fb0cbdc92660eaa5901096bef9ab829cbdf82ae6
Vinayak Gupta <75876908+vinayak700@users.noreply.github.com>
2024-05-21 22:23:32 +0530

    Deploy

---
+++ server/config/connectDB.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "mern-chat-app",
+      writeConcern: { w: "majority" },
+    });
+    console.log("Mongodb connected");
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("Connect to DB");
+    });
+
+    connection.on("error", (error) => {
+      console.log("Something is wrong in mongodb ", error);
+    });
+  } catch (error) {
+    console.log("Something is wrong ", error);
+  }
+}
+
+module.exports = connectDB;
