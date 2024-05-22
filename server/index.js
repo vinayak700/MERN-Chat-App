c69c08afe748fe48af5d75fbdc396dfd9fd6fb0a
Vinayak Gupta <75876908+vinayak700@users.noreply.github.com>
2024-05-22 11:54:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin: ["http://localhost:3000", process.env.FRONTEND_URL],
+    origin: true,
     credentials: true
 }))
 app.use(express.json())
