ddaf0d352f56a8619f0688d3731639ef63494244
Vinayak Gupta <75876908+vinayak700@users.noreply.github.com>
2024-06-05 12:00:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin: true,
-    credentials: true
+    origin : process.env.FRONTEND_URL,
+    credentials : true
 }))
 app.use(express.json())
 app.use(cookiesParser())
